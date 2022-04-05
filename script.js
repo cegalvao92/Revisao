c241a11c3a87f5682c98d2159e660d103f669a29
cegalvao92 <cegalvao13@gmail.com>
2022-04-05 17:32:52 -0300

    Projeto Revisão

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const baseUrl = "http://localhost:3000/paletas";
 
-const findPaletas = async () => {
+async function findAllPaletas() {
   const response = await fetch(`${baseUrl}/find-paletas`);
+
   const paletas = await response.json();
 
   paletas.forEach((paleta) => {
@@ -23,25 +24,25 @@ const findPaletas = async () => {
   });
 };
 
-findPaletas();
+findAllPaletas();
 
 const findPaletaById = async () => {
-    const id = document.getElementById("idPaleta").value;
-  
-    const response = await fetch(`${baseUrl}/find-paleta/${id}`);
-  
-    const paleta = await response.json();
-  
-    const paletaEscolhidaDiv = document.getElementById("paletaEscolhida");
-  
-    paletaEscolhidaDiv.innerHTML = `<div class="PaletaCardItem">
-      <div>
-        <div class="PaletaCardItem__sabor">${paleta.sabor}</div>
-        <div class="PaletaCardItem__preco">R$ ${paleta.preco.toFixed(2)}</div>
-        <div class="PaletaCardItem__descricao">${paleta.descricao}</div>
-      </div>
-        <img class="PaletaCardItem__foto" src=${
-          paleta.foto
-        } alt=${`Paleta de ${paleta.sabor}`} />
-    </div>`;
-  };
+  const id = document.getElementById("idPaleta").value;
+
+  const response = await fetch(`${baseUrl}/find-paleta/${id}`);
+
+  const paleta = await response.json();
+
+  const paletaEscolhidaDiv = document.getElementById("paletaEscolhida");
+
+  paletaEscolhidaDiv.innerHTML = `<div class="PaletaCardItem">
+    <div>
+      <div class="PaletaCardItem__sabor">${paleta.sabor}</div>
+      <div class="PaletaCardItem__preco">R$ ${paleta.preco.toFixed(2)}</div>
+      <div class="PaletaCardItem__descricao">${paleta.descricao}</div>
+    </div>
+      <img class="PaletaCardItem__foto" src=${
+        paleta.foto
+      } alt=${`Paleta de ${paleta.sabor}`} />
+  </div>`;
+};
